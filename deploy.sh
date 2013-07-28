610e8f240ed123301e93b576e94eeb45f946da4d
Ran Tavory <rantav@gmail.com>
2013-07-28 10:19:50 +0300

    Update the deploy script to submit the version file and push code to github

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,9 @@
 now=`date +"%D-%T"`
 v="$(cat version)"
 echo "${v%.*}.$((${v##*.}+1))" > version
+git ci version -m "[deploy] Bump version"
 v="$(cat version)"
 echo deploying new version: $v
 git tag -a v$v -m "Deployment from $now, v$v"
 mrt deploy devdev.io
+git push
