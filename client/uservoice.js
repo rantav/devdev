9b23e15d2481330e8910a6b8e11f9d01577a7e48
Ran Tavory <rantav@gmail.com>
2013-07-28 15:22:54 +0300

    Add a support forum using uservoice https://trello.com/c/AjM8TD4P/2-add-a-feedback-form

---
+++ client/uservoice.js
@@ -0,0 +1,10 @@
+UserVoice = window.UserVoice || [];
+UserVoice.push(['showTab', 'classic_widget', {
+  mode: 'support',
+  primary_color: '#cc6d00',
+  link_color: '#007dbf',
+  tab_label: 'Feedback',
+  tab_color: '#f2c694',
+  tab_position: 'middle-right',
+  tab_inverted: true
+}]);
